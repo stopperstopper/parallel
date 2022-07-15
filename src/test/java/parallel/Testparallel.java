941d0c812c63b226d25ba2e22904f3d968aa029e
Alex Sadykov <92750261+stopperstopper@users.noreply.github.com>
2022-07-15 18:26:29 +0500

    Update Testparallel.java

--- src/test/java/parallel/Testparallel.java
+++ src/test/java/parallel/Testparallel.java
@@ -27,7 +27,7 @@ public class Testparallel {
     capabilities.setCapability("browserVersion", "103.0");
     capabilities.setCapability("enableVNC", true);
     driver = new RemoteWebDriver(
-        URI.create("http://192.168.1.64:4444/wd/hub").toURL(),
+        URI.create("http://localhost:4444/wd/hub").toURL(),
         capabilities
     );
   }
